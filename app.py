cf6972d08e2fb890361fa05611e1c572996438e8
chella kasinath <114824398+KasinathOfficial@users.noreply.github.com>
2025-03-15 14:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
+import seaborn as sns
 
 # Load the dataset
 data_path = 'train.csv'
